430932d1528d6acaa9e075b358537e0e9050585e
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2024-05-16 08:30:20 -0500

    ACT

--- Cargar_Datos.py
+++ Cargar_Datos.py
@@ -10,17 +10,17 @@ class Cls_Cargar_Datos:
     def InicializarDatos(self):
         # Datos proporcionados
         Datos = {
-            "Fijos Alimento Mes": [8739605],
-            "Variable Alimento Mes": [11783432],
-            "Variable Energeticos Alimento Mes": [3904343],
-            "Variable MDO Alimento Res /Mes": [6476254],
-            "Fijos Producción Mes": [53464981],
-            "Variable Producción Mes": [2746430],
+            "Fijos Alimento Mes": [8739605.571],
+            "Variable Alimento Mes": [11783432.33],
+            "Variable Energeticos Alimento Mes": [3904343.863],
+            "Variable MDO Alimento Res /Mes": [6476254.667],
+            "Fijos Producción Mes": [53464981.42],
+            "Variable Producción Mes": [2746430.05],
             "Fijos Energeticos Producción Mes": [3218670],
-            "Variable MDO Producción Mes": [12952509],
+            "Variable MDO Producción Mes": [12952509.33],
             "Fijos Proyectos infraestructura Mes": [19877000],
             "Variable Plan Sanitario 3 meses/res": [9293],
-            "MDO Valor x persona": [3238127]
+            "MDO Valor x persona": [3238127.333]
         }
         return Datos
     
@@ -29,8 +29,8 @@ class Cls_Cargar_Datos:
         # Valores predeterminados
         default_values = {
             'Inventario Reses':2000,
-            'Numero de Reses Salida': 660,
-            'Tiempo permanencia': 95,
+            'Numero de Reses Salida': 10,
+            'Tiempo permanencia': 30.0,
             'Peso Inicial': 200.0,
             'Precio Res Flaca Kg': 2.5,
             'Precio Res Flaca': 500.0,
