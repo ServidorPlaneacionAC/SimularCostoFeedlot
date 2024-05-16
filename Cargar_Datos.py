3091de39aa52d7de1e699ec5ebfe7c3534a1c91f
ServidorPlaneacionAC <132586180+ServidorPlaneacionAC@users.noreply.github.com>
2024-05-16 08:35:24 -0500

    act

--- Cargar_Datos.py
+++ Cargar_Datos.py
@@ -10,17 +10,17 @@ class Cls_Cargar_Datos:
     def InicializarDatos(self):
         # Datos proporcionados
         Datos = {
-            "Fijos Alimento Mes": [8739605.571],
-            "Variable Alimento Mes": [11783432.33],
-            "Variable Energeticos Alimento Mes": [3904343.863],
-            "Variable MDO Alimento Res /Mes": [6476254.667],
-            "Fijos Producción Mes": [53464981.42],
-            "Variable Producción Mes": [2746430.05],
-            "Fijos Energeticos Producción Mes": [3218670],
-            "Variable MDO Producción Mes": [12952509.33],
-            "Fijos Proyectos infraestructura Mes": [19877000],
-            "Variable Plan Sanitario 3 meses/res": [9293],
-            "MDO Valor x persona": [3238127.333]
+            "Fijos Alimento Mes": 8739605.571,
+            "Variable Alimento Mes": 11783432.33,
+            "Variable Energeticos Alimento Mes": 3904343.863,
+            "Variable MDO Alimento Res /Mes": 6476254.667,
+            "Fijos Producción Mes": 53464981.42,
+            "Variable Producción Mes": 2746430.05,
+            "Fijos Energeticos Producción Mes": 3218670,
+            "Variable MDO Producción Mes": 12952509.33,
+            "Fijos Proyectos infraestructura Mes": 19877000,
+            "Variable Plan Sanitario 3 meses/res": 9293,
+            "MDO Valor x persona": 3238127.333
         }
         return Datos
     
